611758d6b6d08f0bdbc587356a096d9e4981e773
Gillian Sabdao <gilliansabdao@gmail.com>
2026-04-01 23:39:52 +1300

    Created New Main Menu Scene
    - Created a new main menu scene with a very basic theme and a pixel font.
      It's a control node instead of a node 2D and uses containers to layout
      the title and buttons.
    - Rearranged Assets folder.
    - Made settings.gd statically declare variable types.
    - Deleted old main menu scene.

--- Autoloads/constants/scene_directory.gd
+++ Autoloads/constants/scene_directory.gd
@@ -4,8 +4,8 @@ const MAIN_SCENE_PATH = "res://Scenes/Main.tscn"
 
 const scene_dir = {
 	"SETTINGS" = "res://Scenes/Settings/Settings.tscn",
-	"MENU" = "res://Scenes/MainMenu/Main.tscn",
 	"GAME" = "res://Scenes/Game/Game.tscn",
+	"MAIN_MENU" = "res://Scenes/Menus/main_menu.tscn",
 }
 
 var current_scene : String = "res://Scenes/Game/test_map.tscn"
--- Scenes/Settings/settings.gd
+++ Scenes/Settings/settings.gd
@@ -12,42 +12,40 @@ func _ready() -> void:
 	
 	# init resolution
 	size_option.get_popup().max_size = POPUP_MAX_SIZE	
-	for r in GuiAutoload.resolutions:
+	for r: String in GuiAutoload.resolutions:
 		size_option.add_item(r)
 	
 	# set default size_option picked item. If there's nothing from config, juse use first
-	var default_size_option_index = get_size_option_id(GuiAutoload.current_resolution)
+	var default_size_option_index: int = get_size_option_id(GuiAutoload.current_resolution)
 	if default_size_option_index == -1:
 		default_size_option_index = 0
 	size_option.selected = default_size_option_index
 	
 	# init window mode
 	window_option.get_popup().max_size = POPUP_MAX_SIZE
-	for m in GuiAutoload.WINDOW_MODE:
-		var mode = GuiAutoload.WINDOW_MODE[m]
+	for m: String in GuiAutoload.WINDOW_MODE:
+		var mode: GuiAutoload.WINDOW_MODE = GuiAutoload.WINDOW_MODE[m]
 		window_option.add_item(GuiAutoload.WINDOW_MODE_TO_STRING[mode])
 	
 	# set default window_option. If there's nothing from config, just query what it is
 	window_option.selected = GuiAutoload.current_window_mode
-	
-	var default_window_mode_option_index = 0
 
-func get_size_option_id(text):
-	for i in range(size_option.item_count):
+func get_size_option_id(text: String) -> int:
+	for i: int in range(size_option.item_count):
 		if size_option.get_item_text(i) == text:
 			return size_option.get_item_id(i)
 	return -1
 
 func _on_exit_button_pressed() -> void:
-	Switcher.change_scene(Directory.scene_dir.MENU)
+	Switcher.change_scene(Directory.scene_dir.MAIN_MENU)
 
 func _on_size_option_item_selected(index: int) -> void:
-	var res_string = size_option.get_item_text(index)
+	var res_string: String = size_option.get_item_text(index)
 	GuiAutoload.change_window_resolution(res_string)
 
 func _on_window_option_item_selected(index: int) -> void:
-	var mode_string = window_option.get_item_text(index)
+	var mode_string: String = window_option.get_item_text(index)
 	GuiAutoload.change_window_mode(mode_string)
 
-func _on_resolution_change(new_res) -> void:
+func _on_resolution_change(new_res: String) -> void:
 	size_option.selected = get_size_option_id(new_res)
